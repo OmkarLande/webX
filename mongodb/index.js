07455f1fd9e6877eda4c921a1d73f512b650310c
Og_Om <84120403+OmkarLande@users.noreply.github.com>
2024-05-16 22:55:56 +0530

    chore: Add Angular ng-if directive and MongoDB connection code

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,11 +1,32 @@
-function f(x) {
-    x = 'x-' + x;
-return function(y) {
-    y = 'y-' + y;
-return function(z) {
-return "z-" + y
+const mongoose = require("mongoose");
+
+// Connection
+const connectWithDb = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/your-db-name");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
 }
-}
-}
-let g = f ("a")("b")("c");
-console.log(g)
+// Schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        min: 8,
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+    },
+})
+
+// Model
+const User = mongoose.model("User", userSchema);
+
